ecf0aa9b38ba8df8a905050fe1fc88b707716ada
Dzoan Duong <doanthanhduong11031990@gmail.com>
2017-09-08 11:07:20 +0700

    change feature 5

--- demo_git_android_studio/app/src/main/java/flow/git/demo/duongmatheo/demogitflow/MainActivity.java
+++ demo_git_android_studio/app/src/main/java/flow/git/demo/duongmatheo/demogitflow/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbarBaby = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbarBaby);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        toolbarBaby.setSubtitleTextColor(Color.YELLOW);
+        toolbar.setSubtitleTextColor(Color.YELLOW);
     }
 
     @Override
