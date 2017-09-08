c105dc37515ab9129b1935d97130751b1856e2d4
hcmut15 <leducanh.bkit10@gmail.com>
2017-09-08 10:23:01 +0700

    init change for git

--- demo_git_android_studio/app/src/main/java/flow/git/demo/duongmatheo/demogitflow/MainActivity.java
+++ demo_git_android_studio/app/src/main/java/flow/git/demo/duongmatheo/demogitflow/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.d("duc_anh","Hello!!!");
     }
 
     @Override
